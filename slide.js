ed6c005864fccbc51adefa862d67762ea9f6feff
Success <olamidejesutofunmi42@gmail.com>
2022-09-10 01:16:13 +0000

    first commit

---
+++ slide.js
@@ -0,0 +1,9 @@
+const sigUpButton = document.getElementById('signUp')
+const signInButton = document.getElementById('signIn')
+const container = document.getElementById('container')
+
+sigUpButton.addEventListener('click', () =>
+container.classList.add('right-panel-active'));
+
+signInButton.addEventListener('click', () =>
+container.classList.remove('right-panel-active'));
